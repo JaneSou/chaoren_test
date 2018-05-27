303e9d27cadf0b03c894a8922a4468ec19ecb2ad
JaneSou <czc0178@qq.com>
2018-05-27 12:52:28 +0800

    test

--- chaoren/plugins/Chatting.py
+++ chaoren/plugins/Chatting.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8-*-
 # 闲聊插件
 
-# try:
-#     reload         # Python 2
-# except NameError:  # Python 3
-#     from importlib import reload
+try:
+    reload         # Python 2
+except NameError:  # Python 3
+    from importlib import reload
 
 import sys
 reload(sys)
